92a05dc82c1bb1f475dde7948d295455fa09aa2b
Yaroslav Dombrovskyi <dombrovskiy91@gmail.com>
2024-08-21 13:39:14 +0200

    Initial project setup
    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // p-4 for test Tailwind
+    <div className="App p-4">
+      Hello
+    </div>
+  );
+}
+
+export default App;
